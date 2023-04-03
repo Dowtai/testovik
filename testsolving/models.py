db92acf4038ea203345859ad3ecf0a2f0008e032
Dowtai <111916661+Dowtai@users.noreply.github.com>
2023-04-03 19:16:43 +0500

    Update models.py

--- testsolving/models.py
+++ testsolving/models.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.utils import timezone
 
 class Test(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.TextField()
     description = models.TextField(blank=True)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
@@ -11,13 +11,13 @@ class Test(models.Model):
         return self.title
 
 class Question(models.Model):
-    text = models.CharField(max_length=255)
+    text = models.TextField()
     test = models.ForeignKey(Test, on_delete=models.CASCADE, related_name='questions')
-    option1 = models.CharField(max_length=255)
-    option2 = models.CharField(max_length=255)
-    option3 = models.CharField(max_length=255)
-    option4 = models.CharField(max_length=255)
-    correct_answer = models.PositiveIntegerField(choices=((1, 'Ответ 1'), (2, 'Ответ 2'), (3, 'Ответ 3'), (4, 'Ответ 4')), blank=True)
+    option1 = models.TextField()
+    option2 = models.TextField()
+    option3 = models.TextField()
+    option4 = models.TextField()
+    correct_answer = models.PositiveIntegerField(choices=((1, 'Ответ 1'), (2, 'Ответ 2'), (3, 'Ответ 3'), (4, 'Ответ 4')), blank=True, null=True)
 
     def __str__(self):
         return self.text
